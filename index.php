14c341a212f1d9668c96402f754aaae3f9537306
William Batsell <76410143+William-Batsell@users.noreply.github.com>
2021-07-16 14:11:47 -0500

    php to html

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
     <a href="logout.php">Logout</a>
     <a href="lobby.php">Create a lobby</a>
     <a href="singleplayer.html">Play Alone!</a>
-    <a href="multiplayer.php">Play With Friends!</a>
+    <a href="multiplayer.html">Play With Friends!</a>
     <h1>Welcome to MovieGuessr</h1>
     <br>
     Hello
