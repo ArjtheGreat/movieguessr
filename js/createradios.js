75060a4b8ea1c72477a31134baa07f0d91df4e8d
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2022-04-06 10:21:55 -0400

    reverted dropdown menu on main branch

--- js/createradios.js
+++ js/createradios.js
@@ -2,7 +2,7 @@ function createGame() {
 
     // assuming csvfile is a long string which is a read version of the text file  i.e.
 
-    // gb,great bri'ain
+    // gb,great bri"ain
     // us,UNited States
     // zh,Algeria
 
@@ -43,174 +43,26 @@ function createGame() {
 
     radiochooser = getRandomInt(0,2);
     console.log("randomint" + radiochooser);
-    const form = document.getElementById("d1");
+    const select = document.getElementById("d1");
 
-    const li1 = document.createElement('li');
-    const input1 = document.createElement('input');
+    const li1 = document.createElement("option");
+    li1.setAttribute("type", "option");
+    li1.setAttribute("value", "United States");
+    li1.setAttribute("name", "United States");
 
-    const li2 = document.createElement('li');
-    const input2 = document.createElement('input');
+    const li2 = document.createElement("option");
+    li2.setAttribute("type", "option");
+    li2.setAttribute("value", "Premium"); 
+    li2.setAttribute("name", "Premium");
+    console.log("premium")
 
-    const li3 = document.createElement('li');
-    const input3 = document.createElement('input');
+    const li3 = document.createElement("option");
+    li3.setAttribute("type", "option");
+    li3.setAttribute("value", "VIP");
 
-  
-if(radiochooser==0) {
-    //radio setup
-    radio1var = randomProperty(dict);
-    console.log(radio1var);
-    radio1text = dict[radio1var];
-    radio2text = "";
-    radio2var = randomProperty(dict);
-    console.log(radio2var);
-    if(radio2var == radio1var) {
-      while(radio2var == radio1var) {
-        radio2var = randomProperty(dict);
-      }
-    }
-    radio2text = dict[radio2var];
-    if(radio1text == chosenmovie.country) {
-      while(radio1text == chosenmovie.country) {
-        radio1var = randomProperty(dict);
-      }
-    }
-    if(radio2text == chosenmovie.country) {
-      while(radio2text == chosenmovie.country) {
-        radio2var = randomProperty(dict);
-      }
-    }
-    
-    input1.setAttribute("type", "radio");
-    input1.setAttribute("value", "1");
-    input1.setAttribute("name", "choice");
-    li1.append(input1);
-    li1.append(radio1text);
-
-
-    input2.setAttribute("type", "radio");
-    input2.setAttribute("value", "2");
-    input2.setAttribute("name", "choice");
-    li2.append(input2);
-    li2.append(radio2text);
-
-
-
-    input3.setAttribute("type", "radio");
-    input3.setAttribute("value", "correct");
-    input3.setAttribute("name", "choice");
-    li3.append(input3);
-    li3.append(chosenmovie.country);
-}
-
-
-if(radiochooser==1) {
-        //radio setup
-    radio3var = randomProperty(dict);
-    console.log(radio3var);
-    radio3text = dict[radio3var];
-    radio2text = "";
-    radio2var = randomProperty(dict);
-    console.log(radio2var);
-    if(radio2var == radio3var) {
-      while(radio2var == radio3var) {
-        radio2var = randomProperty(dict);
-      }
-    }
-    radio2text = dict[radio2var];
-
-    if(radio3text == chosenmovie.country) {
-      while(radio3text == chosenmovie.country) {
-        radio3var = randomProperty(dict);
-      }
-    }
-    if(radio2text == chosenmovie.country) {
-      while(radio2text == chosenmovie.country) {
-        radio2var = randomProperty(dict);
-      }
-    }
-
-    input3.setAttribute("type", "radio");
-    input3.setAttribute("value", "3");
-    input3.setAttribute("name", "choice");
-    li3.append(input3);
-    li3.append(radio3text);
-
-
-    input2.setAttribute("type", "radio");
-    input2.setAttribute("value", "2");
-    input2.setAttribute("name", "choice");
-    li2.append(input2); 
-    li2.append(radio2text);
-
-
-
-    input1.setAttribute("type", "radio");
-    input1.setAttribute("value", "correct");
-    input1.setAttribute("name", "choice");
-    li1.append(input1);
-    li1.append(chosenmovie.country);
-}
-
-
-
-
-
-if(radiochooser==2) {
-    //radio setup
-radio3var = randomProperty(dict);
-console.log(radio3var);
-radio3text = dict[radio3var];
-radio1text = "";
-radio1var = randomProperty(dict);
-console.log(radio1var);
-if(radio1var == radio3var) {
-  while(radio1var == radio3var) {
-    radio1var = randomProperty(dict);
-  }
-}
-radio1text = dict[radio1var];
-
-if(radio1text == chosenmovie.country) {
-  while(radio1text == chosenmovie.country) {
-    radio1var = randomProperty(dict);
-  }
-}
-if(radio3text == chosenmovie.country) {
-  while(radio3text == chosenmovie.country) {
-    radio3var = randomProperty(dict);
-  }
-}
-
-
-
-
-input3.setAttribute("type", "radio");
-input3.setAttribute("value", "3");
-input3.setAttribute("name", "choice");
-li3.append(input3);
-li3.append(radio3text);
-
-
-
-input1.setAttribute("type", "radio");
-input1.setAttribute("value", "1");
-input1.setAttribute("name", "choice");
-li1.append(input1);
-li1.append(radio1text);
-
-
-input2.setAttribute("type", "radio");
-input2.setAttribute("value", "correct");
-input2.setAttribute("name", "choice");
-li2.append(input2);
-li2.append(chosenmovie.country);
-}
-    
-
-
-    form.append(li1);  
-    form.append(li2);  
-    form.append(li3);  
+    select.append(li1);  
+    select.append(li2);  
+    select.append(li3);  
   }
 
   function updateImage() {
@@ -240,7 +92,7 @@ createGamevar.function1();
 
 var submitAnswer = function() {
   console.log("hi");
-  var radios = document.getElementsByName('choice');
+  var radios = document.getElementsByName("choice");
   var value= "";
   for (var i = 0, length = radios.length; i < length; i++) {
       if (radios[i].checked) {
@@ -250,15 +102,15 @@ var submitAnswer = function() {
   }
   
   if (value == "" ) {
-    alert('please select answer');
+    alert("please select answer");
   } else if ( value == "correct" ) {
-    alert('Answer is correct !');
+    alert("Answer is correct !");
     score = score + 1000;
     document.getElementById("score-text").innerHTML = "";
     document.getElementById("score-text").innerHTML = score; 
     console.log(score);
   } else {
-    alert('Answer is wrong');
+    alert("Answer is wrong");
   } 
   clearBox();
   numofquestions = numofquestions-1;
