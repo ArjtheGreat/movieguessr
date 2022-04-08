8eef134d0bcaf70d8215e0caf3e71ca92026bade
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2022-04-07 21:08:14 -0400

    some work

--- imdbapi/database.js
+++ imdbapi/database.js
@@ -1,22 +1,5 @@
-var unirest = require("unirest");
-
-var req = unirest("GET", "https://movie-database-imdb-alternative.p.rapidapi.com/");
-
-req.query({
-	"i": "tt4154796",
-	"type": "movie",
-	"r": "json"
-});
-
-req.headers({
-	"x-rapidapi-key": "5c1ae27e1amsh7317a252884ea16p16a568jsnab9a2ff89e58",
-	"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-	"useQueryString": true
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+function addScript(src) { var s = document.createElement('script'); s.src = src; document.head.appendChild(s); }
+window.imdb$foo = function (results) {
+	/* ... */
+};
+addScript('https://sg.media-imdb.com/suggests/f/foo.json');
--- js/moviedict.js
+++ js/moviedict.js
@@ -28,10 +28,31 @@ var TheMatrix = {
     country: "United States of America"
 }
 
+var ShaunOfTheDead = {
+    name: "Shaun of the Dead",
+    pic: "movies/USA_TheMatrix.png",
+    country: "United Kingdom of Great Britain and Northern Ireland"
+}
+
+var HotFuzz = {
+    name: "Hot Fuzz",
+    pic: "movies/USA_TheMatrix.png",
+    country: "United Kingdom of Great Britain and Northern Ireland"
+}
+
+var TheWorldsEnd = {
+    name: "The World's End",
+    pic: "movies/USA_TheMatrix.png",
+    country: "United Kingdom of Great Britain and Northern Ireland"
+}
+
 
 var movie = []; //this is array literation
 
 movie.push(AvengersEndgame);
 movie.push(TheMatrix);
+movie.push(ShaunOfTheDead);
+movie.push(HotFuzz);
+movie.push(TheWorldsEnd);
 
 movie.push(PulpFiction);
